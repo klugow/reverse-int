29da050d67db523cbb67461cee1be06ab6070e56
Andrey Makarenko <119301983+klugow@users.noreply.github.com>
2023-02-14 11:51:16 +0300

    write js code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+    
+let nonMinus = Math.abs(n);
+let number = String(nonMinus);
+let array = [];
+
+    array.push(number[2]);
+    array.push(number[1]);
+    array.push(number[0]);
+ 
+return parseInt(array[0]+array[1]+array[2])
+
 }
